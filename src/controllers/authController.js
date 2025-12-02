9b3b1e08bb7e6b0c9ed0bc9bd7f70e7e4c4fdf8e
Rati Krishna Moharana <mratikrishna@gmail.com>
2025-12-02 16:08:38 +0530

    Registration and Login added

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,9 +1,39 @@
-
+import jwt from 'jsonwebtoken';
+import User from '../models/userModel.js';
+import bcrypt from 'bcryptjs';
 
 export const registerController = async (req, res) => {
-
-}
+    const { username, password, role } = req.body;
+    // if (!username || !password || !role) {
+    //     return res.status(400).json({ message: 'All fields are required' });
+    // }
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await User.create({ username, password: hashedPassword, role });
+        res.status(201).json({ message: 'User registered successfully' });
+    } catch (error) {
+        res.status(500).json({ message: 'Failed to rgister user', error: error.message });
+    }
+};
 
 export const loginController = async (req, res) => {
-    
-}
+    try {
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
+
+        if(!user) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch) {
+            res.status(401).json({ message: 'Invalid credentials' });
+        }
+
+        const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        res.status(200).json({ message: 'Login successful', user, token });
+    } catch (error) {
+        res.status(500).json({ message: 'Failed to login user', error: error.message });
+    }
+};
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -17,4 +17,6 @@ const userSchema = new mongoose.Schema({
     }
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema);
+const User = mongoose.models.User || mongoose.model('User', userSchema);
+
+export default User;
